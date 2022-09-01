bdfdc2ccb27afb61c2c5c28b0a47c0fb22d79bd1
EthanB21 <112134892+EthanBalon@users.noreply.github.com>
2022-09-01 23:42:03 +0000

    created loading animations

--- main.py
+++ main.py
@@ -1,2 +1,46 @@
+# ------------------------------------------
+#everything that the program needs is imported from here
 import test
+import startup
+import loadinganimation
+import replit
+#-------------------------------------------
+startup.startsequence() #displays the startup sequence on the terminal to the user
+
+question = input('Start the new code test? y/n: ') #when their is new code needing to be test
+if question == "y":
+    loadinganimation.loadinganim() #shows user the beutiful loading animation while giving the code time to load
+    test.testfunction()
+    
+
+else:
+  print('New code test skipped, started program normally')
+  for i in range(3):
+  
+    loadinganimation.loadinganim()
+  replit.clear()
+  print('haha their is nothing here yet')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ startup.py
@@ -0,0 +1,12 @@
+#what will happen when the code starts
+
+
+def startsequence():
+  print('█   █ █▀▀ █   █▀▀ █▀▀█ █▀▄▀█ █▀▀ ')
+  print('█▄█▄█ █▀▀ █   █   █  █ █ ▀ █ █▀▀ ')
+  print(' ▀ ▀  ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀▀ ▀   ▀ ▀▀▀')
+  print('Powered by an ASCII based GUI')
+
+
+
+
